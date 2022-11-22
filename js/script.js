8efb2b8a1d5fc851cc3c4fb5a7d302e053d872df
LucaBonifazio <luca.bonifazio13@gmail.com>
2022-11-22 15:27:55 +0100

    add little style

--- js/script.js
+++ js/script.js
@@ -9,10 +9,12 @@ new Vue({
 			for (let i = 0; i < 10; i++) {
 				axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
 				.then(axiosResponse => {
-					console.log(axiosResponse);
 					this.arrMail.push(axiosResponse.data.response);
 				})
 			}
 		},
+        deleteMail() {
+            this.arrMail = [];
+        }
 	},
 });
