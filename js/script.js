6c1ddfa89fca0323ce6ded7000ff28a242fbd977
LucaBonifazio <luca.bonifazio13@gmail.com>
2022-11-23 10:10:47 +0100

    add bonus

--- js/script.js
+++ js/script.js
@@ -8,9 +8,7 @@ new Vue({
 			this.arrMail = [];
 			for (let i = 0; i < 10; i++) {
 				axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-				.then(axiosResponse => {
-					this.arrMail.push(axiosResponse.data.response);
-				})
+				.then(axiosResponse => (this.arrMail.push(axiosResponse.data.response)));
 			}
 		},
         deleteMail() {
